8aa8c312e877ed2600dd4290d89ab7fcff46ec3a
Yanjun Chen <51148553+markyanjunch@users.noreply.github.com>
2022-03-27 20:37:26 -0400

    up-to-date lec17

--- src/components/EventDateTime.js
+++ src/components/EventDateTime.js
@@ -1,7 +1,13 @@
 import './EventDateTime.css';
+// Luxon is a date/time library we added via `npm install luxon`, you'll see
+// it in package.json. We import the DateTime utility from that library
 import { DateTime } from 'luxon';
 
 const EventDateTime = (props) => {
+    // Converts military time to AM/PM with Luxon. Knowing how to use Luxon
+    // specifically isn't a requirement of the class, this is here as an example
+    // of bringing in an external library to address something your application
+    // needs.
     function convert(input) {
         return DateTime
             .fromFormat(input, 'HH:mm:ss')
@@ -13,6 +19,10 @@ const EventDateTime = (props) => {
             {props.dateStart}
         </div>
         <div className='event__start-end'>
+            {/*
+                We are calling convert() on timeStart and timeEnd props to
+                change the umich default format to AM/PM
+            */}
             { convert(props.timeStart) }-{ convert(props.timeEnd)}
         </div>
     </div>);
--- src/components/EventsListing.js
+++ src/components/EventsListing.js
@@ -4,6 +4,9 @@ import EventInfo from "./EventInfo";
 import EventInstance from "./EventInstance";
 
 const EventsListing = (props) => {
+    // This is in the console so you can see that when App passes in
+    // events as <EventsListing events={events} />, that `events`
+    // data is here in props.events.
     console.log('THE PROPS', props);
 
     /**
@@ -18,27 +21,40 @@ const EventsListing = (props) => {
         // Loop through the event list. Add each event as new array item.
         // If React sees an array of JSX "markup", it will render each one.
         props.events.forEach((eventInstance, index) =>
+            // Add an event's "markup" to the eventsToShow array.
             eventsToShow.push(<EventInstance key={index}>
                     <EventDateTime
                         dateStart={eventInstance.date_start}
                         timeStart={eventInstance.time_start}
                         timeEnd={eventInstance.time_end}
                     />
-                    <EventInfo
-                        title={eventInstance.event_title}
-                        description={eventInstance.description}
-                    />
+                   <EventInfo
+                       title={eventInstance.event_title}
+                       description={eventInstance.description}
+                   />
                 </EventInstance>
             )
         );
+
         return eventsToShow;
+        // What is happening here could be done more concisely with JavaScripts's map()
+        // function (so `props.events.map()`), but that can be harder to read
+        // for those not used to it, so we're using forEach() and push() instead.
     }
 
-    return (
-        <div className='events'>
-            {generateEvents()}
-        </div>
-    );
+    if (props.events.length > 0) {
+        return (
+            <div className='events'>
+                {/*
+               The events "markup" is generated in a function (above)
+               to make this return statement easier to read.
+            */}
+                {generateEvents()}
+            </div>
+        );
+    } else {
+        return <h2 className='events--loading'>LOADING!!!!</h2>
+    }
 }
 
 export default EventsListing;
